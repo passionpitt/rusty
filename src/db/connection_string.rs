73c7ce33720d9e26e08c65056cabbc2a272f30cf
thisismindo <mindosilalahi@gmail.com>
2023-03-06 16:41:43 -0800

    update db connection string

--- src/db/connection_string.rs
+++ src/db/connection_string.rs
@@ -2,13 +2,16 @@
 pub struct ConnectionString {
     pub user: String,
     pub password: String,
-    pub host: String
+    pub host: String,
+    pub db_name: String
 }
 
 impl ConnectionString {
     pub fn fetch_connection_string() -> String {
         let db_user: String = std::env::var("DB_USER").expect("DB_USER must be set.");
         let db_password: String = std::env::var("DB_PASSWORD").expect("DB_PASSWORD must be set.");
-        return format!("mysql://{}:{}@localhost/rusty", db_user, db_password);
+        let db_host: String = std::env::var("DB_HOST").expect("DB_HOST must be set.");
+        let db_name: String = std::env::var("DB_NAME").expect("DB_NAME must be set.");
+        return format!("mysql://{}:{}@{}/{}", db_user, db_password, db_host, db_name);
     }
 }
