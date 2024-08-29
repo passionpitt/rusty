4efd7288f2e29ab66780f64404e2c0bed0953708
thisismindo <mindosilalahi@gmail.com>
2024-08-29 11:24:34 -0700

    remove old setup

--- setup/database.sql
+++
@@ -1,9 +0,0 @@
-USE `db`;
-
-CREATE TABLE `rusty`.`users` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(64) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NULL,
-  `email` VARCHAR(64) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NULL,
-);
-
-INSERT INTO `db`.`users` SET name = "mindo", email = "thisismindo@foobar.net";
